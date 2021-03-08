8699f73de7d2632cfbca5cd8d03ce8f02f045fef
drdr xp <drdr.xp@gmail.com>
2021-03-08 10:36:16 +0800

    release: v0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="k3log",
     packages=["k3log"],
-    version="0.1.3",
+    version="0.1.4",
     license='MIT',
     description='k3log is a collection of log utilities.',
     long_description='# k3log\n\n[![Build Status](https://travis-ci.com/pykit3/k3log.svg?branch=master)](https://travis-ci.com/pykit3/k3log)\n![Python package](https://github.com/pykit3/k3log/workflows/Python%20package/badge.svg)\n[![Documentation Status](https://readthedocs.org/projects/k3log/badge/?version=stable)](https://k3log.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3log)](https://pypi.org/project/k3log)\n\nk3log is a collection of log utilities.\n\nk3log is a component of [pykit3] project: a python3 toolkit set.\n\n\nk3log is a collection of log utilities for logging.\n\n\n\n# Install\n\n```\npip install k3log\n```\n\n# Synopsis\n\n```python\n\n# make a file logger in one line\nlogger = pk3logutil.make_logger(\'/tmp\', level=\'INFO\', fmt=\'%(message)s\',\n                                datefmt="%H:%M:%S")\nlogger.info(\'foo\')\n\nlogger.stack_str(fmt="{fn}:{ln} in {func}\\n  {statement}", sep="\\n")\n# runpy.py:174 in _run_module_as_main\n#   "__main__", fname, loader, pkg_name)\n# runpy.py:72 in _run_code\n#   exec code in run_globals\n# ...\n# test_logutil.py:82 in test_deprecate\n#   pk3logutil.deprecate()\n#   \'foo\', fmt=\'{fn}:{ln} in {func}\\n  {statement}\', sep=\'\\n\')\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3',
